a11eff602cb22f56346fa9c33d4cb5ab041a1355
Spraynard <kellan.martin@gmail.com>
2019-01-09 21:49:50 -0500

    Finishing up implementation of Counting Sort

--- ch_8/countingsort.c
+++ ch_8/countingsort.c
@@ -1,4 +1,5 @@
 void Counting_Sort( int *A, int inputLen, int *B, int k ) {
+	k = k + 1;
 	int C[k], i, j;
 
 	for ( i = 0; i < k; i++ )
@@ -6,19 +7,30 @@ void Counting_Sort( int *A, int inputLen, int *B, int k ) {
 		C[i] = 0;
 	}
 
+	// C is filled with 0's
+
 	for ( j = 0; j < inputLen; j++ )
 	{
 		C[A[j]] = C[A[j]] + 1;
 	}
 
-	for ( i = 0; i < k; i++ )
+	// C[i] is filled where i = # of times the number shows up
+
+	for ( i = 1; i < k; i++ )
 	{
-		C[i] = C[i] - C[i -1];
+		C[i] = C[i] + C[i - 1];
 	}
+
+	// C[i] is filled where i = # of elements < i
 
 	for ( j = inputLen; j > 0; j-- )
 	{
-		B[C[A[j]]] = A[j];
-		C[A[j]] = C[A[j]] - 1;
+		B[C[A[j - 1]] - 1] = A[j - 1];
+
+		// C[A[j - 1]] is the correct final position of A[j] in the output array.
+		// We decrement in order to assure that non distinct numbers are placed in B as well.
+		C[A[j - 1]] = C[A[j - 1]] - 1;
 	}
+
+	// Should be sorted now.
 }
--- ch_8/main.c
+++ ch_8/main.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 void printArray( int *inputArray, size_t size )
 {
 	if ( ! size )
@@ -25,21 +23,17 @@ void printArray( int *inputArray, size_t size )
 
 int main() {
 	int array[] = { 2, 5, 3, 0, 2, 3, 0, 3 };
+	int array2[] = { 2, 2000, 0, 417, 80, 81, 64, 34 };
+	int array3[] = { 6, 0, 2, 0, 1, 3, 4, 6, 1, 3, 2 };
+
 	int arrayLen = sizeof(array)/sizeof(array[0]);
+	int array3Len = sizeof(array3)/sizeof(array3[0]);
 
 	int *outArray = (int *) calloc( arrayLen, sizeof( int ) );
+	int *outArray3 = (int *) calloc( array3Len, sizeof( int ) );
 
-	printf("Input Array Before: \n");
-	printArray(array, arrayLen);
-
-	printf("Output Array Before: \n");
-	printArray(outArray, arrayLen);
-
-	Counting_Sort( array, arrayLen, outArray, 5 );
-
-	printf("Input Array After: \n");
-	printArray(array, arrayLen);
+	Counting_Sort( array3, array3Len, outArray3, 6 );
 
 	printf("Output Array After: \n");
-	printArray(outArray, arrayLen);
+	printArray(outArray3, array3Len);
 }
